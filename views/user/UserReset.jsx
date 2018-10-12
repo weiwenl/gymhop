407dbdd223919e1382fd70ce4aa9b4b6599faa79
weiwenl <weiwenl.justin@gmail.com>
2018-10-12 13:50:42 +0800

    User reset page to get user new pw

---
+++ views/user/UserReset.jsx
@@ -0,0 +1,27 @@
+var React = require("react");
+var DefaultLayout = require('../layout/Default');
+
+class UserReset extends React.Component {
+  render() {
+
+    return (
+      <DefaultLayout title="Reset my Password">
+          <div className="UserReset-page">
+              <form method="POST" action="/user/reset">
+              <h1>Changing your password, { this.props.cookie }? </h1>
+              <div>
+                <h3>New Password</h3> <input name="newpassword" type="text" />
+              </div>
+              <input type="submit" value="Confirm" />
+            </form>
+          <footer>
+              {/* <h3>Designed by climbers, made for climbers</h3>
+              <h3>Terms etc ...</h3> */}
+          </footer>
+          </div>
+       </DefaultLayout>
+    );
+  }
+}
+
+module.exports = UserReset;
