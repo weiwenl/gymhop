b362cde66244c8f8d2da5e2ddab6597609223e2b
weiwenl <weiwenl.justin@gmail.com>
2018-11-30 01:48:21 +0800

    styling user account

--- views/user/UserAccount.jsx
+++ views/user/UserAccount.jsx
@@ -4,8 +4,7 @@ var DefaultLayout = require('../layout/Default');
 
 class useraccount extends React.Component {
   render() {
-
-    let userInfo = this.props.obj.data.map((info) => {
+  let userInfo = this.props.obj.data.map((info) => {
              return (
                 <tr>
                    <td key={info.gym_name}>
@@ -24,35 +23,69 @@ class useraccount extends React.Component {
     return (
       //Welcome user's name
       <DefaultLayout title= {this.props.obj.user}>
-          <div className="useraccount-page">
-            <a href="#">Edit Profile</a>
-            <a href="/account/logout">Logout</a>
-            <div>
-              <h3>Total Passes</h3><h3>{this.props.obj.sum}</h3>
+        <div className="useraccount">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="">
+            <div className="container">
+              <span className="navbar-brand"><a className="text-white" href="/account/user">GYM hop</a></span>
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+              </button>
+
+              <div>
+                <ul className="navbar-nav ml-auto">
+                  <li>
+                    <text className="nav-link">Welcome, {this.props.obj.user}</text>
+                  </li>
+                  {/* <li>
+                    <a className="nav-link" href="#editprofile">Edit Profile</a>
+                  </li> */}
+                  <li>
+                    <a className="nav-link" href="/user/checkin">Gym Check-In</a>
+                  </li>
+                  <li>
+                    <a className="nav-link" href="/account/logout">Log Out</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <img src="/dp.jpg"></img>
-            <h3>{ this.props.obj.user }</h3>
+          </nav>
 
-            <table>
-                <tr>
-                  <th>Gym</th>
-                  <th>Quantity</th>
-                  <th>Expiry Date</th>
-                </tr>
+          <div className="container">
 
-                  {userInfo}
+              <div className="d-flex justify-content-center align-items-end">
+                <div className="card">
 
-            </table>
+                  <div className="card-header">
+                    <img src="../boy.jpg" width="80vw" className="avatar rounded-circle"></img>
+                    <h2 className="avatar-name">{this.props.obj.user}</h2>
+                  </div>
 
-            <div>
-                <a href="/user/add">Add Passes</a>
-                <a href="/user/checkin">Check into gym</a>
+                  <div className="card-body">
+                    <table className="table">
+                      <thead className="thead">
+                        <tr>
+                          <th scope="col">Gym</th>
+                          <th scope="col">Quantity</th>
+                          <th scope="col">Expiry Date</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {userInfo}
+                      </tbody>
+                    </table>
+                  </div>
+
+                  <div className="card-footer">
+                    <div className="d-flex justify-content-center links">
+                      <a className="btn btn-primary" href="/user/add" role="button">Add Passes</a>
+
+                    </div>
+                </div>
             </div>
-
-          <footer>
-
-          </footer>
           </div>
+        </div>
+
+      </div>
        </DefaultLayout>
     );
   }
