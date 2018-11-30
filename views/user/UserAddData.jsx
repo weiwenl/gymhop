9450366dc99961993b39336a075cbba13e4763b0
weiwenl <weiwenl.justin@gmail.com>
2018-11-30 14:28:03 +0800

    styling user add passes and user check in

--- views/user/UserAddData.jsx
+++ views/user/UserAddData.jsx
@@ -5,35 +5,75 @@ class useradddata extends React.Component {
   render() {
     return (
       <DefaultLayout title="Add more passes">
-          <div className="AddForm-page">
-            <form method="POST" action="/user">
-            <h3>Gym Name</h3>
-                <select name="gym">
-                    <option value="Boulder Movement">Boulder Movement</option>
-                    <option value="Boulder World">Boulder World</option>
-                    <option value="Boulder +">Boulder +</option>
-                    <option value="Boys Town Climbing">Boys Town Climbing Wall</option>
-                    <option value="Climb Central">Climb Central</option>
-                    <option value="Climbers Laboratory">Climbers Laboratory</option>
-                    <option value="Fit Bloc">Fit · Bloc</option>
-                    <option value="GroundUp Climbing">GroundUp Climbing</option>
-                    <option value="Kinetics Climbing">Kinetics Climbing</option>
-                    <option value="Onsight Climbing">Onsight Climbing</option>
-                    <option value="Oyeyo Climbing">Oyeyo Climbing</option>
-                    <option value="Safra Yishun Climbing">Safra Yishun Climbing</option>
-                    <option value="The Cliff">The Cliff</option>
-                    <option value="The Rock School">The Rock School</option>
-                    <option value="Upside Boulders">Upside Boulders</option>
-                </select>
-            <h3>Number of Passes</h3> <input name="quantity" type="text" pattern="\d*" maxLength="2" />
-            <h3>Expiry Date</h3> <input  name="expiry" type="date" />
+        <div className="userdata">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="">
+            <div className="container">
+              <span className="navbar-brand"><a className="text-white" href="/account/user">GYM hop</a></span>
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+              </button>
 
-            <input type="submit" value="Add Passes" />
-            </form>
-          <footer>
+              <div>
+                <ul className="navbar-nav ml-auto">
+                  <li>
+                    <text className="nav-link">Welcome, {this.props.username}</text>
+                  </li>
+                  {/* <li>
+                    <a className="nav-link" href="#editprofile">Edit Profile</a>
+                  </li> */}
+                  <li>
+                    <a className="nav-link" href="/user/checkin">Gym Check-In</a>
+                  </li>
+                  <li>
+                    <a className="nav-link" href="/account/logout">Log Out</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
 
-          </footer>
-          </div>
+          <div className="container">
+
+              <div className="d-flex justify-content-center align-items-end">
+                <div className="card">
+
+                  <div className="card-header">
+                    <img src="../boy.jpg" width="80vw" className="avatar rounded-circle"></img>
+                    <h2 className="avatar-name">{this.props.username}</h2>
+                  </div>
+
+                  <div className="card-body">
+                    <form method="POST" action="/user">
+                    <h6>Gym Name</h6>
+                        <select name="gym">
+                            <option value="Boulder Movement">Boulder Movement</option>
+                            <option value="Boulder World">Boulder World</option>
+                            <option value="Boulder +">Boulder +</option>
+                            <option value="Boys Town Climbing">Boys Town Climbing Wall</option>
+                            <option value="Climb Central">Climb Central</option>
+                            <option value="Climbers Laboratory">Climbers Laboratory</option>
+                            <option value="Fit Bloc">Fit · Bloc</option>
+                            <option value="GroundUp Climbing">GroundUp Climbing</option>
+                            <option value="Kinetics Climbing">Kinetics Climbing</option>
+                            <option value="Onsight Climbing">Onsight Climbing</option>
+                            <option value="Oyeyo Climbing">Oyeyo Climbing</option>
+                            <option value="Safra Yishun Climbing">Safra Yishun Climbing</option>
+                            <option value="The Cliff">The Cliff</option>
+                            <option value="The Rock School">The Rock School</option>
+                            <option value="Upside Boulders">Upside Boulders</option>
+                        </select>
+                    <h6>Number of Passes</h6> <input name="quantity" type="text" pattern="\d*" maxLength="2" />
+                    <h6>Expiry Date</h6> <input  name="expiry" type="date" />
+
+                    <input class="add-btn btn btn-warning" type="submit" value="Add Passes" />
+                    </form>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+        </div>
        </DefaultLayout>
     );
   }
--- views/user/UserCheckIn.jsx
+++ views/user/UserCheckIn.jsx
@@ -4,33 +4,71 @@ var DefaultLayout = require('../layout/Default');
 class usercheckin extends React.Component {
   render() {
     return (
-      <DefaultLayout title="Gym check-in">
-          <div className="removeForm-page">
-            <form method="POST" action="">
-            <h3>Gym Name</h3>
-            <select name="gym">
-                <option value="Boulder Movement">Boulder Movement</option>
-                <option value="Boulder World">Boulder World</option>
-                <option value="Boulder +">Boulder +</option>
-                <option value="Boys Town Climbing">Boys Town Climbing Wall</option>
-                <option value="Climb Central">Climb Central</option>
-                <option value="Climbers Laboratory">Climbers Laboratory</option>
-                <option value="Fit Bloc">Fit · Bloc</option>
-                <option value="GroundUp Climbing">GroundUp Climbing</option>
-                <option value="Kinetics Climbing">Kinetics Climbing</option>
-                <option value="Onsight Climbing">Onsight Climbing</option>
-                <option value="Oyeyo Climbing">Oyeyo Climbing</option>
-                <option value="Safra Yishun Climbing">Safra Yishun Climbing</option>
-                <option value="The Cliff">The Cliff</option>
-                <option value="The Rock School">The Rock School</option>
-                <option value="Upside Boulders">Upside Boulders</option>
-            </select>
+      <DefaultLayout title="Gym Check-In">
+        <div className="userCheckin">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="">
+            <div className="container">
+              <span className="navbar-brand"><a className="text-white" href="/account/user">GYM hop</a></span>
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+              </button>
 
-            <input type="submit" value="Check In" />
-            </form>
-          <footer>
+              <div>
+                <ul className="navbar-nav ml-auto">
+                  <li>
+                    <text className="nav-link">Welcome, {this.props.username}</text>
+                  </li>
+                  {/* <li>
+                    <a className="nav-link" href="#editprofile">Edit Profile</a>
+                  </li> */}
+                  <li>
+                    <a className="nav-link" href="/user/checkin">Gym Check-In</a>
+                  </li>
+                  <li>
+                    <a className="nav-link" href="/account/logout">Log Out</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
 
-          </footer>
+          <div className="container">
+
+              <div className="d-flex justify-content-center align-items-end">
+                <div className="card">
+
+                  <div className="card-header">
+                    <img src="../boy.jpg" width="80vw" className="avatar rounded-circle"></img>
+                    <h2 className="avatar-name">{this.props.username}</h2>
+                  </div>
+
+                  <div className="card-body">
+                    <form method="POST" action="">
+                    <h3>Gym Name</h3>
+                    <select name="gym">
+                        <option value="Boulder Movement">Boulder Movement</option>
+                        <option value="Boulder World">Boulder World</option>
+                        <option value="Boulder +">Boulder +</option>
+                        <option value="Boys Town Climbing">Boys Town Climbing Wall</option>
+                        <option value="Climb Central">Climb Central</option>
+                        <option value="Climbers Laboratory">Climbers Laboratory</option>
+                        <option value="Fit Bloc">Fit · Bloc</option>
+                        <option value="GroundUp Climbing">GroundUp Climbing</option>
+                        <option value="Kinetics Climbing">Kinetics Climbing</option>
+                        <option value="Onsight Climbing">Onsight Climbing</option>
+                        <option value="Oyeyo Climbing">Oyeyo Climbing</option>
+                        <option value="Safra Yishun Climbing">Safra Yishun Climbing</option>
+                        <option value="The Cliff">The Cliff</option>
+                        <option value="The Rock School">The Rock School</option>
+                        <option value="Upside Boulders">Upside Boulders</option>
+                    </select>
+
+                    <input class="checkin-btn btn btn-success" type="submit" value="Check In" />
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
        </DefaultLayout>
     );
